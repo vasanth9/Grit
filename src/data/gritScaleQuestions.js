a6d7f114d1d48bd4674aabecb1807c5c80873a8b
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-03-12 06:51:38 +0530

    grit scale data 🖲️

---
+++ src/data/gritScaleQuestions.js
@@ -0,0 +1,54 @@
+export const gritScaleQuestions = [
+  {
+    question:
+      "New Ideas and projects sometimes distract me from previous ones.",
+    order: 1,
+  },
+  {
+    question: "Setbacks don't discourage me. I don't give up easily.",
+    order: 0,
+  },
+  {
+    question: "I often set a goal but later choose to pursue a different one.",
+    order: 1,
+  },
+  {
+    question: "I am a hard worker.",
+    order: 0,
+  },
+  {
+    question:
+      "I have difficulty maintaining my focus on projects that take more than a few months to complete.",
+    order: 1,
+  },
+  {
+    question: "I finish whatever I begin.",
+    order: 0,
+  },
+  {
+    question: "My interests change from year to year.",
+    order: 1,
+  },
+  {
+    question: "I am diligent. I never give up.",
+    order: 0,
+  },
+  {
+    question:
+      "I have been obsessed with a certain ideas or project for a short time but later lost interest.",
+    order: 1,
+  },
+  {
+    question: "I have overcome setbacks to conquer an important challenge.",
+    order: 0,
+  },
+];
+// order=0 => 1,2,3,4,5
+// order=1 => 5,4,3,2,1
+export const gritScaleOptions = [
+  "Not at all like me",
+  "Not much like me",
+  "Some-what like me",
+  "Mostly like me",
+  "Very much like me",
+];
