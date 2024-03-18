1bcf3ef2215bcb48d6934502d3b1046a1f49661e
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-03-18 22:12:43 +0530

    display all questions

---
+++ src/components/Question.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { gritScaleQuestions, gritScaleOptions } from '../data/gritScaleQuestions.js';
+const props = defineProps({
+  index: Number
+})
+</script>
+
+<template>
+  <h1>{{ gritScaleQuestions[index].question }}</h1>
+  <button v-for="option in gritScaleOptions">
+    {{ option }}
+  </button>
+</template>
+
+<style scoped>
+h1 {
+  text-align: left;
+  color: #378CE7;
+}
+
+button {
+  display: block;
+  margin: 10px;
+  color: #DFF5FF;
+  background-color: #5356FF;
+}
+</style>
---
+++ src/pages/GritScale.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Question from '../components/Question.vue';
+</script>
+<template>
+    <div v-for="n in 10">
+        <Question :index="n - 1" />
+    </div>
+</template>
